8b38a5ffa2755cf696051ba2c46787bdf0c16008
Antonio Piccolboni <antonio@piccolboni.info>
2013-08-22 11:14:16 -0700

    functions inspired by sql

---
+++ tests/sqlish.R
@@ -0,0 +1,27 @@
+# Copyright 2013 Revolution Analytics
+#    
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+stopifnot(
+	all(
+		(
+			function(){
+				s = 5
+				envir = sys.frame(sys.nframe())
+				as.data.frame(where(input(mtcars), cyl>s, carb < s, envir = envir))})() ==
+			subset(mtcars, subset = cyl > 5 & carb < 5)))
+
+stopifnot(
+	all(
+		(function(){as.data.frame(select(input(mtcars), cyl))})() ==
+			subset(mtcars, select = cyl)))

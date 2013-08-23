7d2c3a9ba9d6f57b9fa198412a6fe8babf579616
Antonio Piccolboni <antonio@piccolboni.info>
2013-08-22 22:35:07 -0700

    collected test cases for evaluation issues

---
+++ tests/non-standard-eval.R
@@ -0,0 +1,35 @@
+rm(mtcars)
+mtcars = cbind(model = rownames(mtcars), mtcars)
+param = 4
+as.data.frame(transform(input(mtcars), fourcarb = carb/param))
+rm(param)
+#(function(){param = 4; as.data.frame(transform(input(mtcars), fourcarb = carb/param))})()
+param = 4
+as.data.frame(subset(input(mtcars), carb > param))
+rm(param)
+#(function(){param = 4; as.data.frame(subset(input(mtcars), carb > param))})()
+param = 4
+as.data.frame(select(input(mtcars), fourcarb = carb/param))
+rm(param)
+(function(){param = 4; as.data.frame(select(input(mtcars), fourcarb = carb/param))})()
+param = 4
+as.data.frame(where(input(mtcars), carb > param))
+rm(param)
+(function(){param = 4; as.data.frame(where(input(mtcars), carb > param))})()
+param = 4
+as.data.frame(mutate(input(mtcars), fourcarb = carb/param))
+rm(param)
+#(function(){param = 4; as.data.frame(mutate(input(mtcars), fourcarb = carb/param))})()
+param = 4
+as.data.frame(summarize(input(mtcars), fourcarb = carb/param))
+rm(param)
+#(function(){param = 4; as.data.frame(summarize(input(mtcars), fourcarb = carb/param))})()
+param = "mpg"
+mmtcars = as.data.frame(melt(input(mtcars), c("model", param)))
+mmtcars
+rm(param)
+#(function(){param = "mpg"; as.data.frame(melt(input(mtcars), c("model", param)))})()
+param = formula(model ~ variable)
+as.data.frame(dcast(input(mmtcars), param))
+rm(param)
+(function(){param = formula(model ~ variable); as.data.frame(dcast(input(mmtcars), model ~ variable))})()

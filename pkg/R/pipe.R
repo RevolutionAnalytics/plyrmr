bd8bc184ef2eabe11d97f91d1e8c3e047531ea47
Antonio Piccolboni <antonio@piccolboni.info>
2013-11-01 14:16:00 -0700

    catch the null case

--- pkg/R/pipe.R
+++ pkg/R/pipe.R
@@ -37,7 +37,7 @@ make.map.fun =
 			k = {	
 				if(is.null(keyf)) k 
 				else safe.cbind(k, keyf(w))}
-			if(nrow(w) > 0) keyval(k, w)}}
+			if(!is.null(w) && nrow(w) > 0) keyval(k, w)}}
 
 make.reduce.fun = 
 	function(valf, ungroup) 

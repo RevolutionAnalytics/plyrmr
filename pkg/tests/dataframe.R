99ec0beb1000ae7e16e2d6d2ef7f226c3b94e086
Antonio Piccolboni <antonio@piccolboni.info>
2014-04-14 14:09:04 -0700

    tests for dataframe functions

--- pkg/tests/dataframe.R
+++ pkg/tests/dataframe.R
@@ -1,4 +1,4 @@
-# Copyright 2013 Revolution Analytics
+# Copyright 2014 Revolution Analytics
 #    
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,65 @@
 # limitations under the License.
 
 library(plyrmr)
+library(quickcheck)
+library(functional)
 
-stopifnot(
-	all(
-		(function(){s = 5; where(mtcars, cyl > s & carb < s)})() ==
-			subset(mtcars, subset = cyl > 5 & carb < 5)))
+cmp.df = plyrmr:::cmp.df
 
-stopifnot(
-	all(
-		(function(){transmute(mtcars, cyl)})() ==
-			subset(mtcars, select = cyl)))
+#where
+
+unit.test(
+	function(df) {
+		numeric.cols = which(sapply(df, is.numeric ))
+		filter.col = numeric.cols[1]
+		filter = parse(text=paste(filter.col, ">0"))
+		cmp.df(
+			where(df, filter),
+      subset(df, eval(filter, envir = list2env(df))))},
+	list(rdata.frame),
+	precondition =
+		function(df) 
+			any(sapply(df, is.numeric)))
+
+
+#transmute
+
+unit.test(
+	function(df) {
+		col = as.name(sample(names(df), 1))
+		cmp.df(
+			transmute(df, col),
+		  select(df, col))},
+	list(rdata.frame))
+
+#bind.cols
+
+unit.test(
+	function(df) {
+		col = as.name(sample(names(df), 1))
+		cmp.df(
+			bind.cols(df, z = col),
+			transform(df, z = eval(col)))},
+	list(rdata.frame))
+
+#sample
+
+args = 	
+	list(
+		any = list(n = Curry(rinteger, len.lambda = 0)),
+		Bernoulli = list(p = Curry(rdouble, lambda = 0, min =0, max = 1)),
+		hypergeometric = list(n = Curry(rinteger, len.lambda = 0)))
+
+for(method in c("any", "Bernoulli", "hypergeometric")) {
+	method.args = args[[method]] 
+	unit.test(
+		function(df, ...) 
+			cmp.df(
+				df,
+				union(do.call(sample, c(list(df, method = method), list(...))), df)),
+		c(list(rdata.frame), method.args),
+	precondition = 
+		function(df, ...) {
+			if(is.element(method, c("any", "hypergeometric")))
+				list(...)$n <= nrow(df)
+			else TRUE})}

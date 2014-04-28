5571f3ec8b91529d4f35def67ae5362665e7f652
Antonio Piccolboni <antonio@piccolboni.info>
2014-04-28 16:08:22 -0700

    a different plyrmr backed by  SparkR
    first simple gapply running

--- pkg/R/pipe.R
+++ pkg/R/pipe.R
@@ -13,82 +13,15 @@
 # limitations under the License.
 
 # function manip
+.options = new.env()
+.options$context = sparkR.init()
 
-comp = 
-	function(...) {
-		funs = list(...)
-		funs = strip.nulls(funs)
-		if(length(funs) == 1)
-			funs[[1]]
-		else		
-			do.call(Compose, funs)}
+set.context =
+	function()
+		.options$context = sparkR.init()
 
-drop.gather = 
-	function(x){
-		if(is.element(".gather", names(x))) {
-			x = x[, !(names(x) == ".gather"), drop = FALSE]
-			if (ncol(x) * nrow(x) == 0) NULL else x}
-		else
-			x}
-
-make.task.fun = 																					# this function is a little complicated so some comments are in order
-	function(keyf, valf, ungroup, ungroup.args, vectorized) {												# make a valid map function from two separate ones for keys and values
-		if(is.null(valf))                                   # the value function defaults to identity
-			valf = identity 
-		function(k, v) {                                    # this is the signature of a correct map function
-			rownames(k) = NULL                                # wipe row names unless you want them to count in the grouping (Hadoop only sees serialization)
-			if(vectorized) {
-				w = valf(drop.gather(safe.cbind.kv(k, v)))
-				k = w[, names(k)]}
-			else
-				w = safe.cbind.kv(k,	valf(drop.gather(safe.cbind.kv(k, v))))       # pass both keys and values to val function as a single data frame, then make sure we keep keys for the next step
-      stopifnot(!(ungroup && is.null(keyf)))
-			k = {
-				if (ungroup) { 					# if ungroup called select or reset keys, otherwise accumulate
-					if(length(ungroup.args) == 0) 
-						NULL            
-					else
-						do.call(select, c(list(k), lapply(ungroup.args, function(a) as.call(list(as.name("-"), a)))))}
-				else {	
-					if(is.null(keyf)) k 														# by default keep grouping whatever it is
-					else safe.cbind(k, keyf(drop.gather(w)))}}										# but if you have a key function, use it and cbind old and new keys
-			if(!is.null(w) && nrow(w) > 0) keyval(k, drop.gather(w))}}     # special care for empty cases
-
-make.map.fun = 
-	function(keyf, valf)
-		make.task.fun(keyf, valf, ungroup = FALSE, ungroup.args = NULL, vectorized = FALSE)
-
-make.reduce.fun = 
-	function(valf, ungroup, ungroup.args, vectorized) 
-		make.task.fun(NULL, valf, ungroup, ungroup.args, vectorized)
-
-make.combine.fun = 
-	function(valf, vectorized) {
-		cf  = make.task.fun(NULL, valf, ungroup = FALSE, ungroup.args = NULL, vectorized = vectorized)
-		function(k, v) {
-			retval = cf(k, v)
-			nm = sapply(names(v), function(col) grep(paste0(col), names(retval$val), value=T))
-			mn = names(nm)
-			names(mn) = nm
-			new.names = mn[names(retval$val)]
-			mask = !is.na(new.names)
-			names(retval$val)[mask] = new.names[mask]
-			retval}}
 
 #pipes
-
-is.data = 
-	function(x)
-		inherits(x, "pipe")
-
-as.character.pipe = 
-	function(x, ...) 
-		paste(
-			"Slots set:", 
-			paste(names(unclass(x)), collapse = ", "), "\n",
-			"Input:",
-			paste(as.character(x[["input"]]), collapse = ","),
-			"\n")
 
 print.pipe =
 	function(x, ...) {
@@ -123,25 +56,85 @@ is.vectorized =
 	function(f) 
 		default(attr(f, "vectorized", exact=TRUE), FALSE)
 
-gapply =
-	function(.data, .f, ...){
-		.f = freeze.env(.f)
-		f1 = make.f1(.f, ...)
-		if(is.null(.data$group)) #still map phase
-			.data$map = comp(.data$map, f1)
-		else { #reduce phase
-			if(!is.mergeable(.f) && 
-				 	is.null(.data$reduce) && 
-				 	default(.data$recursive.group, FALSE))
-				stop("Did you try to combine a gather with a non-mergeable operation?")
-			if(is.mergeable(.f) && is.null(.data$reduce)) { #can use combiner
-				.data$recursive.group = TRUE
-				.data$combine = f1}
-			.data$reduce = comp(.data$reduce, f1)
-			.data$vectorized =
-				default(.data$vectorized, TRUE) &&
-				is.vectorized(.f)}
-		.data}
+keycols = 
+	function(kv)
+		attributes(kv)[['keys']]
+
+set.keycols = 
+	function(kv, keycols) {
+		attr(kv, 'keys') = keycols
+		kv}
+
+add.keycols = 
+	function(kv, keycols) {
+		attr(kv, 'keys') = union(keycols(kv), keycols)
+		kv}
+
+rm.keycols = 
+	function(kv, keycols) {
+		attr(kv, 'keys') = setdiff(keycols(kv), keycols)
+		kv}
+
+keys = function(kv) kv[, keycols(kv), drop = FALSE]
+values = function(kv) kv[, setdiff(names(kv), keycols(kv))]
+
+keyval = 
+	function (key, val = NULL) {
+		if (missing(val)) 
+			keyval(key = NULL, val = key)
+		else set.keycols(recycle.keyval(key, val), names(key))}
+
+recycle.keyval = 
+	function (k, v) {
+		if(is.null(k))
+			v
+		else {
+			if ((nrow(k) == nrow(v)) || is.null(k)) 
+				cbind(k, v)
+			else {
+				if (nrow(v) == 0) 
+					NULL
+				else {
+					as.data.frame(do.call(cbind, c(k, v)))}}}}
+
+rdd.list2kv =
+	function(ll) 
+		do.call(
+			rbind, 
+			lapply(
+				ll,
+				function(l) {
+					if(is.data.frame(l)) l 
+					else {
+						if(is.data.frame(l[[2]]))
+							l[[2]]
+						else
+							do.call(rbind, l[[2]])}}))
+
+kv2rdd.list = 
+	function(kv) {
+		k = keys(kv)
+		if(ncol(k) == 0)
+			list(kv)
+		else
+			mapply(
+				function(x, y) list(k = x, v = y), 
+				lapply(
+					split(k, k, drop = TRUE), function(x) digest(unique(x))), 
+				split(kv, k, drop = TRUE), 
+				SIMPLIFY = FALSE)}
+
+
+gapply = 
+	function(.data, .f, ...) 
+		as.pipe(
+			lapplyPartition(
+				rdd(.data), 
+				function(x) {
+					kv = rdd.list2kv(x)
+					k = keys(kv)
+					kv = make.f1(.f, ...)(kv)
+					kv2rdd.list(safe.cbind.kv(k, kv))}))
 
 group = 
 	function(.data, ..., .envir = parent.frame()) {
@@ -152,22 +145,22 @@ group =
 			function(.y) 
 				do.call(CurryHalfLazy(transmute, .envir = .envir), c(list(.y), dot.args)))}
 
-group.f = 
+group.f =
 	function(.data, .f, ...) {
-		.f = freeze.env(.f)
 		f1 = make.f1(.f, ...)
-		if(is.null(.data$group)) { #ungrouped
-			.data$group = f1}
-		else {
-			if(is.null(.data$reduce)){ #refine grouping with no mr job
-				prev.group = .data$group
-				.data$group = function(v) safe.cbind(prev.group(v), f1(v))}
-			else #run and apply grouping
-				.data = 
-				group.f(
-					input(run(.data, input.format = "native")), 
-					f1)}
-		.data}
+		as.pipe(
+			groupByKey(
+				lapplyPartition(
+					rdd(.data),
+					function(x) {
+						kv = rdd.list2kv(x)
+						k = keys(kv)
+						new.keys = f1(kv)
+						kv2rdd.list(
+							keyval(
+								safe.cbind(k, new.keys), 
+								safe.cbind(v, v[, setdiff(names(x), new.keys)])))}),
+			10L))} #TODO: review constant here
 
 ungroup = 
 	function(.data, ...) {
@@ -199,63 +192,6 @@ is.grouped =
 	function(.data)
 		!is.null(.data[["group"]])
 
-mr.options = 
-	function(.data, ...) {
-		args = list(...)
-		.data[names(args)] = args
-		.data }
-
-mrexec =
-	function(mr.args, input.format) #this is not the input format for the run but the one to encapsulate with the result to read it later
-		as.big.data(
-			do.call(mapreduce, mr.args),
-			format = input.format)
-
-run = 
-	function(.data, input.format = NULL, ...) { #this is not the input format for the run but the one to encapsulate with the result to read it later
-		dof = default(.data[['output.format']], "native")
-		if(is.null(input.format)) {
-			if(is.character(dof))
-				input.format = dof
-			else
-				stop("need to specify input format corresponding to custom output format")}
-		pipe = .data
-		if(
-			all(
-				sapply(
-					pipe[qw(map, reduce, group)], 
-					is.null))) { 
-			if(!is.null(pipe[["output"]])) {
-				dfs.mv(pipe[["input"]]$data, pipe[["output"]])
-				as.big.data(pipe[["output"]], pipe[["input"]]$format)}
-			else {
-				pipe[["input"]]}}
-		else {
-			mr.args = list()
-			mr.args$input = pipe$input$data
-			mr.args$input.format = pipe$input$format
-			mr.args$output = pipe[['output']]
-			mr.args$output.format = pipe[['output.format']]
-			mr.args = strip.nulls(mr.args)
-			mr.args$map = 
-				make.map.fun(
-					keyf = pipe$group, 
-					valf = pipe$map)
-			if(!is.null(pipe$reduce)) {
-				stopifnot(!is.null(pipe$group))
-				mr.args$reduce = 
-					make.reduce.fun(
-						valf = pipe$reduce, 
-						ungroup = default(pipe$ungroup, FALSE),
-						ungroup.args = pipe$ungroup.args,
-						vectorized = default(pipe$vectorized, FALSE))
-				mr.args$vectorized.reduce = pipe$vectorized}
-			if(!is.null(pipe$recursive.group) &&
-				 	pipe$recursive.group) {
-				mr.args$combine =
-					make.combine.fun(pipe$combine, default(pipe$vectorized, FALSE))}
-			mrexec(mr.args, input.format)}}
-
 output = 
 	function(.data, path = NULL, format = "native", input.format = format) {
 		if(missing(input.format) && !is.character(format))
@@ -264,33 +200,32 @@ output =
 		.data[["output"]] = path
 		as.big.data(.data, input.format)}
 
-as.big.data.pipe = run
 
 as.pipe = function(x, ...) UseMethod("as.pipe")
 
-as.pipe.big.data = 
+as.pipe.RDD = 
 	function(x, ...) 
 		structure(
-			strip.null.args(
-				input = x,
-				ungroup = FALSE),
+			list(rdd = x),
 			class = "pipe")
+
+rdd = 
+	function(x) 
+		x[["rdd"]]
 
 as.pipe.data.frame = 
 	function(x, ...) 
-		as.pipe(as.big.data(x, "native"))
+		as.pipe(
+			parallelize(
+				.options$context, 
+				kv2rdd.list(
+					keyval(x))))
 
-as.pipe.character = 
-	as.pipe.function = 
-	function(x, format = "native", ...) 
-		as.pipe(as.big.data(x, format))
-
-as.pipe.list = Compose(as.big.data, as.pipe)
 
 as.data.frame.pipe =
 	function(x, ...)
 		as.data.frame(
-			as.big.data(x))
+			SparkR::collect(rdd(x)))
 
 input = as.pipe
 
